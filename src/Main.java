cfe42d13b594bbffde1c97c0a52641947d3637be
79774 <Cinnabon_09@.com>
2025-01-12 22:24:50 +0300

    hw done!

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,15 @@
 public class Main {
-
     public static String calculateLeapYear(int i) {
         if (i % 4 == 0 && i % 100 != 0 || i % 400 == 0) {
             return i + " год - високосный";
         } else {
             return i + " год - не високосный";
-
         }
+
     }
 
-    public static String checkMonth(int clientOs) {
-        int clientDeviceYear = 2014;
 
+    public static String checkMonth(int clientOs,int clientDeviceYear ) {
         if (clientOs == 0 && clientDeviceYear < 2015) {
             return "Установите облегченную версию приложения для iOS по ссылке";
         } else if (clientOs == 0 && clientDeviceYear >= 2015) {
@@ -24,7 +22,8 @@ public class Main {
 
     }
 
-    public static String printName(int deliveryDistance){
+
+    public static String calculateDeliveryDays(int deliveryDistance) {
 
         if (deliveryDistance < 20) {
             return "Потребуется 1 день на доставку ";
@@ -34,9 +33,28 @@ public class Main {
         }
         if (deliveryDistance >= 60 && deliveryDistance < 100) {
             return "Потребуется 3 дня на доставку ";
-        }else {
+        } else {
             return "Нет доставки";
         }
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println("Задание 1");
+        int year = 2020;
+        String result1 = calculateLeapYear(year);
+        System.out.println(result1);
+        System.out.println("Задание 2");
+        int clientDeviceYear = 2014;
+        int clientOs = 0;
+        String result2 = checkMonth(clientOs,clientDeviceYear);
+        System.out.println(result2);
+        System.out.println("Задание 3");
+        int deliveryDistance= 30;
+        String result3 =calculateDeliveryDays( deliveryDistance);
+        System.out.println(result3);
+
+
     }
 }
 
